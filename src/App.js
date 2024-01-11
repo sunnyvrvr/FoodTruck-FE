83065d17a9ee952491b78c397a95934504f43f80
sangyeon <asylee02@gmail.com>
2024-01-12 02:42:47 +0900

    facvicon 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='h-screen'>
         <Outlet />
+        <Footer/>
     </div>
   );
 }
